039ba49c9a92d8011ec1f6d6e22f6c95fd8e309d
miguel <miguel.ram.dev01@gmail.com>
2023-05-18 08:01:51 -0500

    feat: project configuration

---
+++ src/functions/hello/handler.ts
@@ -0,0 +1,15 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+import { formatJSONResponse } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+
+import schema from './schema';
+
+const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  console.log(event.headers);
+  return formatJSONResponse({
+    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+    event,
+  });
+};
+
+export const main = middyfy(hello);
