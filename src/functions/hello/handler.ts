ed333a6e4c096a46c2db4af827942c37d38b70b6
miguel <miguel.ram.dev01@gmail.com>
2023-05-18 09:41:39 -0500

    feat: add offline lib and eslint and prettier

--- src/functions/hello/handler.ts
+++ src/functions/hello/handler.ts
@@ -1,15 +1,14 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { formatJSONResponse } from '@libs/api-gateway';
+import { formatJSONException, formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  console.log(event.headers);
-  return formatJSONResponse({
-    message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-    event,
-  });
+    return formatJSONResponse({
+      message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
+      // event,
+    });
 };
 
 export const main = middyfy(hello);
